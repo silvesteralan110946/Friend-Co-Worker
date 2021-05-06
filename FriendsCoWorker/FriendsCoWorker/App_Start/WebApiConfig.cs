dbb3667ec15548c8d5edf683747d6a63a2bd95a2
Alan Silvester <alan.twister5@gmail.com>
2021-05-06 10:30:42 -0300

    Token finalizado

--- FriendsCoWorker/FriendsCoWorker/App_Start/WebApiConfig.cs
+++ FriendsCoWorker/FriendsCoWorker/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FriendsCoWorker.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -13,6 +14,8 @@ namespace FriendsCoWorker
 
             // Rutas de API web
             config.MapHttpAttributeRoutes();
+
+            config.MessageHandlers.Add(new TokenValidationHandler());
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
--- FriendsCoWorker/Frontend-FriendCoWorker/src/app/services/tipodni.service.ts
+++ FriendsCoWorker/Frontend-FriendCoWorker/src/app/services/tipodni.service.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TipoDni } from '../Models/dni.model';
 
-const url = 'https://localhost:44386/api/tipodni/';
+const url = 'https://localhost:44393/api/tipodni/';
+
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
